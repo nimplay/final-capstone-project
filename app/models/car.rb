8c1118ea24fcb8dc9808bf0ad955f7d1cbedcf89
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-25 09:16:09 +0430

    Update a validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,7 @@ class Car < ApplicationRecord
 
   validates :model_name, presence: true
   validates :image, presence: true
+  validates :booked, presence: true
   validates :driver_name, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { greater_than: 0 }
 end
