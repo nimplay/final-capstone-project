dc97bf8c8f4d9f75903cf4f41b03747a525fe393
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-25 08:50:11 +0430

    Add validation and relation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :bookings
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
 end
