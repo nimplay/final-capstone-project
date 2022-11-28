a3bd9179456daef52d8bd6d24c0a5f2b18db1ab9
nimplay <nimrod7day@gmail.com>
2022-11-28 10:59:16 -0400

    test 11

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-puts 'Creating user'
-userslist = User.create([
-                            { name: 'lola',
-                              email:'lola@email.com',
-                              role: 'admin',
-                              password: '123456',
-                              password_confirmation: '123456'
-                            },
-                            { name: 'pepe',
-                              email:'pepe@email.com',
-                              role: 'user',
-                              password: '123456',
-                              password_confirmation: '123456',
-                            },
-                          ])
-puts 'Users created'
+# puts 'Creating user'
+# userslist = User.create([
+#                            { name: 'lola',
+#                              email:'lola@email.com',
+#                              role: 'admin',
+#                              password: '123456',
+#                              password_confirmation: '123456'
+#                            },
+#                            { name: 'pepe',
+#                              email:'pepe@email.com',
+#                              role: 'user',
+#                              password: '123456',
+#                              password_confirmation: '123456',
+#                            },
+#                         ])
+# puts 'Users created'
 puts 'Creating Tests'
 testlist = Test.create([
                             { name: 'Hello React Rails' },
