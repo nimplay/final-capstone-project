2ed2ae94b555b5a4e6aa82aae0d693e88b35027b
wdavidcch <david.13.2121@gmail.com>
2022-12-01 12:31:38 -0400

    set actions

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,22 +1,60 @@
 class Api::V1::BookingsController < ApplicationController
+  # load_and_authorize_resource
+  # before_action :authenticate_user!
+  before_action :set_user
+  before_action :set_booking, only: %i[show update destroy]
+
+  # GET /bookings
   def index
-    @bookings = Booking.where(user_id: 2)
-    render json: @bookings.to_json, status: :ok
+    @bookings = Booking.where(user: @user)
+
+    render json: @bookings
   end
 
+  # GET /bookings/1
   def show
-    @booking = Booking.first
+    render json: @booking
   end
 
-  def new
-    @booking = Booking.new
-  end
-
+  # POST /bookings
   def create
-    @booking = Booking.new
+    @user.bookings.new(booking_params)
+
+    if @booking.save
+      render json: @booking, status: :created
+      # , location: api_v1_booking_url(@booking)
+    else
+      render json: @booking.errors, status: :unprocessable_entity
+    end
   end
 
-  def delete; end
+  # PATCH/PUT /bookings/1
+  def update
+    if @booking.update(booking_params)
+      render json: @booking
+    else
+      render json: @booking.errors, status: :unprocessable_entity
+    end
+  end
 
-  def update; end
+  # DELETE /bookings/1
+  def destroy
+    @booking.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def booking_params
+    params.require(:booking).permit(:date, :date_return, :city, :car_id)
+  end
 end
